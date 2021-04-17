5ba02fcf9f9aa8862b0025842d64281b48011694
rahabwambui <wambui@student.moringaschool.com>
2021-04-17 20:41:04 +0300

    toggle 'What We Do' description for each icon

--- js/script.js
+++ js/script.js
@@ -2,5 +2,17 @@ $(document).ready(function (e) {
     $('.col-sm-4').mouseover(function () {
         $('.Image-Overlay', this).show();
     })
+
+    $('#design').click(function () {
+        $('#design-showing').css('display', 'block');
+    })
+
+    $('#development').click(function () {
+        $('#development-showing').css('display', 'block');
+    })
+
+    $('#productManagement').click(function () {
+        $('#product-management-showing').css('display', 'block');
+    })
 });
 
